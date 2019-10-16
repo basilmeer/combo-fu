f2c40e898b1deedd16c7efc18c621979fc977dba
Basil Meer <hello@basilmeer.com>
2019-10-16 12:39:19 +0500

    Added a hardcoded character list for DBFZ for testing

---
+++ client/src/components/DBFZ/CharacterList.js
@@ -0,0 +1,374 @@
+const DBFZ_CHAR_LIST = [
+  {
+    name: "Android 16",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Android 17",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Android 18",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Android 21",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Bardock",
+    health: "10000",
+    role: "Point, Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Beerus",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Broly",
+    health: "10000",
+    role: "Point, Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Captain Ginyu",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Cell",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Cooler",
+    health: "10000",
+    role: "Point, Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Frieza",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Gogeta (SSGSS)",
+    health: "10000",
+    role: "Point, Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Son Gohan (Teen)",
+    health: "10000",
+    role: "Any",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Son Gohan (Adult)",
+    health: "10000",
+    role: "Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Goku (Base)",
+    health: "10000",
+    role: "Middle, Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Goku (SS)",
+    health: "10000",
+    role: "Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Goku (SSGSS)",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Goku (GT)",
+    health: "10000",
+    role: "Middle, Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Goku Black",
+    health: "10000",
+    role: "Point, Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Gotenks",
+    health: "10000",
+    role: "Any",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Hit",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Janemba",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Jiren",
+    health: "10000",
+    role: "Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Kid Buu",
+    health: "10000",
+    role: "Any",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Krillin",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Majin Buu",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Nappa",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Piccolo",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Tien Shinhan",
+    health: "10000",
+    role: "Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Trunks",
+    health: "10000",
+    role: "Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Vegeta (Base)",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Vegeta (SS)",
+    health: "10000",
+    role: "Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Vegeta (SSGSS)",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Vegito (SSGSS)",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Videl",
+    health: "10000",
+    role: "Point",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Yamcha",
+    health: "10000",
+    role: "Anchor",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  },
+  {
+    name: "Zamasu (Fused)",
+    health: "10000",
+    role: "Point, Middle",
+    game: "Dragon Ball FighterZ",
+    image: "",
+    movelist: [],
+    posted_by: "Bagul",
+    posted_on: "2019-08-17T11:59:09.489Z"
+  }
+];
+
+export default DBFZ_CHAR_LIST;
