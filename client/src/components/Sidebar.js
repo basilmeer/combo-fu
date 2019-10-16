49deb2d88b461af6042876bb53136527ec96001e
Basil Meer <hello@basilmeer.com>
2019-10-16 14:06:27 +0500

    Added a welcome text for authenticated users

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { H2, Menu, MenuDivider, MenuItem, Icon, Intent } from '@blueprintjs/core';
+import { H6, Menu, MenuDivider, MenuItem, Icon, Intent } from '@blueprintjs/core';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 class Sidebar extends Component {
   render() {
     return(
       <div className="cf-sidebar">
-        <span className="cf-sidebar-title">Combo-Fu</span>
+        <div className="cf-sidebar-title">Combo-Fu</div>
         {/* <MenuDivider /> */}
+        <div className="cf-sidebar-welcome">Hi there, Bagul!</div>
         <Menu>
           <NavLink activeClassName={Intent.PRIMARY} to="/">
             <MenuItem text="Home" icon="home" />
@@ -18,14 +19,14 @@ class Sidebar extends Component {
           <NavLink activeClassName={Intent.PRIMARY} to="/profile/">
             <MenuItem text="Profile" icon="user" />
           </NavLink>
-          {/* <NavLink activeClassName={Intent.PRIMARY} to="/explore/">
-            <MenuItem text="Explore" icon="geosearch" />
-          </NavLink> */}
           <NavLink activeClassName={Intent.PRIMARY} to="/login/">
             <MenuItem text="Login" icon="log-in" />
           </NavLink>
           <NavLink activeClassName={Intent.PRIMARY} to="/register/">
             <MenuItem text="Register" icon="person" />
+          </NavLink>
+          <NavLink activeClassName={Intent.PRIMARY} to="/settings/">
+            <MenuItem text="Settings" icon="cog" />
           </NavLink>
           <NavLink activeClassName={Intent.PRIMARY} to="/logout/">
             <MenuItem text="Logout" icon="log-out" />
