945ed6f6bc462496dba9ba56d223cade3ebd1f6d
Basil Meer <hello@basilmeer.com>
2019-10-16 14:08:23 +0500

    Added in the missing square brackets for array based 'characters' output in Game's model

--- db/models/Game.js
+++ db/models/Game.js
@@ -8,9 +8,9 @@ const GameSchema = new Schema({
     type: String,
     required: true
   },
-  characters: {
+  characters: [{
     type: Array
-  },
+  }],
   platforms: [{
     type: Array,
     required: true
