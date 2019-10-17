c1e6f2b6d524cbea82060d31ab8d3a1d259b8d88
Basil Meer <hello@basilmeer.com>
2019-10-17 21:43:26 +0500

    Login form now console logs the token after verifying user

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { H2, FormGroup, InputGroup, Button, Classes } from '@blueprintjs/core';
+import axios from 'axios';
 
-class Signin extends React.Component {
+const initialState = {
+  email: "",
+  password: "",
+  loading: false
+}
+
+class Signin extends Component {
+  state = {
+    email: "",
+    password: "",
+    loading: false
+  }
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  }
+
+  handleChange = e => {
+    const { name, value }= e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let { email, password } = this.state;
+    this.setState({ loading: true });
+    // axios.get('http://localhost:5000/api/users').then(res => console.log(res)).catch(err => console.log(err));
+    axios.post('http://localhost:5000/login', { email, password })
+      .then(res => {
+        console.log(res);
+        this.setState({ loading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
+    this.clearState();
+  }
+  
   render() {
     return (
       <div>
         <H2>Login</H2>
-        <FormGroup
-          helperText="e.g. hello@domainname.com"
-          label="Email Address"
-          labelFor="text-input"
-          labelInfo="(required)"
-        >
-          <InputGroup id="text-input" type="email" />
-        </FormGroup>
-        <FormGroup
-          label="Password"
-          labelFor="text-input"
-          labelInfo="(required)"
-        >
-          <InputGroup id="text-input" type="password" />
-        </FormGroup>
-        <Button className={Classes.BUTTON} intent="success">Login</Button>
+        <form onSubmit={async (e) => await this.handleSubmit(e)}>
+          <FormGroup
+            helperText="e.g. hello@domainname.com"
+            label="Email Address"
+            labelFor="text-input"
+            labelInfo="(required)"
+          >
+            <InputGroup id="text-input" name="email" value={this.state.email} type="email" onChange={this.handleChange} />
+          </FormGroup>
+          <FormGroup
+            label="Password"
+            labelFor="text-input"
+            labelInfo="(required)"
+          >
+            <InputGroup id="text-input" name="password" value={this.state.password} type="password" onChange={this.handleChange} />
+          </FormGroup>
+          <Button type="submit" className={Classes.BUTTON} loading={this.state.loading} intent="success">Login</Button>
+        </form>
       </div>);
   }
 }
